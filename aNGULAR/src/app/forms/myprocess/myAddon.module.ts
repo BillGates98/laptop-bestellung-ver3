76aaafd366a8abaa904da707796c7d78c2280875
ejemaster <ejanguejuniorhotmail.de>
2020-09-12 20:39:05 +0200

    Angular

---
+++ aNGULAR/src/app/forms/myprocess/myAddon.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { startNewProcessComponent } from './startNewProcess.component';
+import { ApproveDataTaskComponent } from './approveDataTask.component';
+
+
+@NgModule({
+  entryComponents: [startNewProcessComponent,ApproveDataTaskComponent],
+  declarations: [startNewProcessComponent,ApproveDataTaskComponent],
+  imports: [FormsModule],
+  exports: [startNewProcessComponent,ApproveDataTaskComponent]
+})
+export class MyAddonModule {}
+
+export { startNewProcessComponent } from './startNewProcess.component';
+export { ApproveDataTaskComponent } from './approveDataTask.component';
